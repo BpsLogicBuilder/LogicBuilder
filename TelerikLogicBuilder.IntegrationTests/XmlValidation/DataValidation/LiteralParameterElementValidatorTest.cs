7736f764bebfbcbe86b7a0742b001fa59c2ba042
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-03-21 13:50:30 -0400

    ILiteralParameterElementValidator.

--- TelerikLogicBuilder.IntegrationTests/XmlValidation/DataValidation/LiteralParameterElementValidatorTest.cs
+++ TelerikLogicBuilder.IntegrationTests/XmlValidation/DataValidation/LiteralParameterElementValidatorTest.cs
@@ -1,29 +1,331 @@
-﻿using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.XmlValidation.DataValidation;
+﻿using ABIS.LogicBuilder.FlowBuilder.Configuration;
+using ABIS.LogicBuilder.FlowBuilder.Enums;
+using ABIS.LogicBuilder.FlowBuilder.Exceptions;
+using ABIS.LogicBuilder.FlowBuilder.Intellisense.Constructors;
+using ABIS.LogicBuilder.FlowBuilder.Intellisense.Parameters;
+using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces;
+using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.Configuration;
+using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.Reflection;
+using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.XmlValidation.DataValidation;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using TelerikLogicBuilder.IntegrationTests.Constants;
 using Xunit;
 
 namespace TelerikLogicBuilder.IntegrationTests.XmlValidation.DataValidation
 {
-    public class LiteralParameterElementValidatorTest
+    public class LiteralParameterElementValidatorTest : IClassFixture<LiteralParameterElementValidatorFixture>
     {
-        public LiteralParameterElementValidatorTest()
-        {
-            serviceProvider = ABIS.LogicBuilder.FlowBuilder.Program.ServiceCollection.BuildServiceProvider();
-        }
+        private readonly LiteralParameterElementValidatorFixture _fixture;
 
-        #region Fields
-        private readonly IServiceProvider serviceProvider;
-        #endregion Fields
+        public LiteralParameterElementValidatorTest(LiteralParameterElementValidatorFixture fixture)
+        {
+            _fixture = fixture;
+        }
 
         [Fact]
         public void CanLiteralParameterElementValidator()
         {
             //arrange
-            ILiteralParameterElementValidator xmlValidator = serviceProvider.GetRequiredService<ILiteralParameterElementValidator>();
+            ILiteralParameterElementValidator xmlValidator = _fixture.ServiceProvider.GetRequiredService<ILiteralParameterElementValidator>();
 
             //assert
             Assert.NotNull(xmlValidator);
         }
+
+        [Fact]
+        public void CanValidateLiteralParameterElement()
+        {
+            //arrange
+            ILiteralParameterElementValidator xmlValidator = _fixture.ServiceProvider.GetRequiredService<ILiteralParameterElementValidator>();
+            var applicationTypeInfo = _fixture.ApplicationTypeInfoManager.GetApplicationTypeInfo(_fixture.ConfigurationService.GetSelectedApplication().Name);
+            XmlElement xmlElement = GetXmlElement(@"<literalParameter name=""stringProperty"">AAA</literalParameter>");
+            List<string> errors = new();
+            LiteralParameter parameter = new
+            (
+                "stringProperty",
+                false,
+                "",
+                LiteralParameterType.String,
+                LiteralParameterInputStyle.SingleLineTextBox,
+                true,
+                false,
+                false,
+                "",
+                "",
+                "",
+                new List<string>(),
+                _fixture.ContextProvider
+            );
+
+            //act
+            xmlValidator.Validate(parameter, xmlElement, applicationTypeInfo, errors);
+
+            //assert
+            Assert.False(errors.Any());
+        }
+
+        [Fact]
+        public void ValidateThrowsForInvalidElementType()
+        {
+            //arrange
+            ILiteralParameterElementValidator xmlValidator = _fixture.ServiceProvider.GetRequiredService<ILiteralParameterElementValidator>();
+            var applicationTypeInfo = _fixture.ApplicationTypeInfoManager.GetApplicationTypeInfo(_fixture.ConfigurationService.GetSelectedApplication().Name);
+            XmlElement xmlElement = GetXmlElement(@"<literalListParameter name=""Includes"">
+                                                        <literalList literalType=""String"" listType=""GenericList"" visibleText=""www"">
+                                                        </literalList>
+                                                      </literalListParameter>");
+            List<string> errors = new();
+            LiteralParameter parameter = new
+            (
+                "stringProperty",
+                false,
+                "",
+                LiteralParameterType.String,
+                LiteralParameterInputStyle.SingleLineTextBox,
+                true,
+                false,
+                false,
+                "",
+                "",
+                "",
+                new List<string>(),
+                _fixture.ContextProvider
+            );
+
+            //act
+            Assert.Throws<CriticalLogicBuilderException>(() => xmlValidator.Validate(parameter, xmlElement, applicationTypeInfo, errors));
+        }
+
+        private static XmlElement GetXmlElement(string xmlString)
+            => GetXmlDocument(xmlString).DocumentElement!;
+
+        private static XmlDocument GetXmlDocument(string xmlString)
+        {
+            XmlDocument xmlDocument = new();
+            xmlDocument.LoadXml(xmlString);
+            return xmlDocument;
+        }
+    }
+
+    public class LiteralParameterElementValidatorFixture : IDisposable
+    {
+        public LiteralParameterElementValidatorFixture()
+        {
+            ServiceProvider = ABIS.LogicBuilder.FlowBuilder.Program.ServiceCollection.BuildServiceProvider();
+            ConfigurationService = ServiceProvider.GetRequiredService<IConfigurationService>();
+            XmlElementValidator = ServiceProvider.GetRequiredService<IXmlElementValidator>();
+            ContextProvider = ServiceProvider.GetRequiredService<IContextProvider>();
+            AssemblyLoadContextService = ServiceProvider.GetRequiredService<IAssemblyLoadContextManager>();
+            LoadContextSponsor = ServiceProvider.GetRequiredService<ILoadContextSponsor>();
+            TypeLoadHelper = ServiceProvider.GetRequiredService<ITypeLoadHelper>();
+            ApplicationTypeInfoManager = ServiceProvider.GetRequiredService<IApplicationTypeInfoManager>();
+
+            ConfigurationService.ProjectProperties = new ProjectProperties
+            (
+                "Contoso",
+                @"C:\ProjectPath",
+                new Dictionary<string, Application>
+                {
+                    ["app01"] = new Application
+                    (
+                        "App01",
+                        "App01",
+                        "Contoso.Test.Flow.dll",
+                        $@"{TestFolders.TestAssembliesFolder}\Contoso.Test.Flow\bin\Debug\netstandard2.0",
+                        ABIS.LogicBuilder.FlowBuilder.Enums.RuntimeType.NetCore,
+                        new List<string>(),
+                        "Contoso.Test.Flow.FlowActivity",
+                        "",
+                        "",
+                        new List<string>(),
+                        "",
+                        "",
+                        "",
+                        "",
+                        new List<string>(),
+                        new WebApiDeployment("", "", "", "", ContextProvider),
+                        ContextProvider
+                    )
+                },
+                new HashSet<string>(),
+                ContextProvider
+            );
+
+            ConfigurationService.ConstructorList = new ConstructorList
+            (
+                new Dictionary<string, Constructor>
+                {
+                    ["TestResponseA"] = new Constructor
+                    (
+                        "TestResponseA",
+                        "Contoso.Test.Business.Responses.TestResponseA",
+                        new List<ParameterBase>
+                        {
+                            new LiteralParameter
+                            (
+                                "stringProperty",
+                                false,
+                                "",
+                                LiteralParameterType.String,
+                                LiteralParameterInputStyle.SingleLineTextBox,
+                                true,
+                                false,
+                                true,
+                                "",
+                                "",
+                                "",
+                                new List<string>(),
+                                ContextProvider
+                            )
+                        },
+                        new List<string>(),
+                        "",
+                        ContextProvider
+                    ),
+                    ["TestResponseB"] = new Constructor
+                    (
+                        "TestResponseB",
+                        "Contoso.Test.Business.Responses.TestResponseB",
+                        new List<ParameterBase>
+                        {
+                            new LiteralParameter
+                            (
+                                "stringProperty",
+                                false,
+                                "",
+                                LiteralParameterType.String,
+                                LiteralParameterInputStyle.SingleLineTextBox,
+                                true,
+                                false,
+                                true,
+                                "",
+                                "",
+                                "",
+                                new List<string>(),
+                                ContextProvider
+                            ),
+                            new LiteralParameter
+                            (
+                                "intProperty",
+                                false,
+                                "",
+                                LiteralParameterType.Integer,
+                                LiteralParameterInputStyle.SingleLineTextBox,
+                                true,
+                                false,
+                                true,
+                                "",
+                                "",
+                                "",
+                                new List<string>(),
+                                ContextProvider
+                            )
+                        },
+                        new List<string>(),
+                        "",
+                        ContextProvider
+                    ),
+                    ["TestResponseC"] = new Constructor
+                    (
+                        "TestResponseC",
+                        "Contoso.Test.Business.Responses.TestResponseC",
+                        new List<ParameterBase>
+                        {
+                            new ObjectParameter
+                            (
+                                "objectProperty",
+                                false,
+                                "",
+                                "System.Object",
+                                true,
+                                false,
+                                true,
+                                ContextProvider
+                            )
+                        },
+                        new List<string>(),
+                        "",
+                        ContextProvider
+                    ),
+                    ["GenericResponse"] = new Constructor
+                    (
+                        "GenericResponse",
+                        "Contoso.Test.Business.Responses.GenericResponse`2",
+                        new List<ParameterBase>
+                        {
+                            new GenericParameter
+                            (
+                                "aProperty",
+                                false,
+                                "",
+                                "A",
+                                ContextProvider
+                            ),
+                            new GenericParameter
+                            (
+                                "bProperty",
+                                false,
+                                "",
+                                "B",
+                                ContextProvider
+                            )
+                        },
+                        new List<string> { "A", "B" },
+                        "",
+                        ContextProvider
+                    ),
+                    ["TypeNotFoundConstructor"] = new Constructor
+                    (
+                        "TypeNotFoundConstructor",
+                        "Contoso.Test.Business.Responses.TypeNotFoundConstructor",
+                        new List<ParameterBase>
+                        {
+                            new LiteralParameter
+                            (
+                                "stringProperty",
+                                false,
+                                "",
+                                LiteralParameterType.String,
+                                LiteralParameterInputStyle.SingleLineTextBox,
+                                true,
+                                false,
+                                true,
+                                "",
+                                "",
+                                "",
+                                new List<string>(),
+                                ContextProvider
+                            )
+                        },
+                        new List<string>(),
+                        "",
+                        ContextProvider
+                    )
+                },
+                new TreeFolder("root", new List<string>(), new List<TreeFolder>())
+            );
+
+            LoadContextSponsor.LoadAssembiesIfNeeded();
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            LoadContextSponsor.UnloadAssembliesOnCloseProject();
+            Assert.Empty(AssemblyLoadContextService.GetAssemblyLoadContext().Assemblies);
+        }
+
+        internal IServiceProvider ServiceProvider;
+        internal IConfigurationService ConfigurationService;
+        internal IXmlElementValidator XmlElementValidator;
+        internal IContextProvider ContextProvider;
+        internal IAssemblyLoadContextManager AssemblyLoadContextService;
+        internal ILoadContextSponsor LoadContextSponsor;
+        internal ITypeLoadHelper TypeLoadHelper;
+        internal IApplicationTypeInfoManager ApplicationTypeInfoManager;
     }
 }
--- TelerikLogicBuilder/Services/XmlValidation/DataValidation/LiteralParameterElementValidator.cs
+++ TelerikLogicBuilder/Services/XmlValidation/DataValidation/LiteralParameterElementValidator.cs
@@ -1,5 +1,7 @@
-﻿using ABIS.LogicBuilder.FlowBuilder.Intellisense.Parameters;
+﻿using ABIS.LogicBuilder.FlowBuilder.Constants;
+using ABIS.LogicBuilder.FlowBuilder.Intellisense.Parameters;
 using ABIS.LogicBuilder.FlowBuilder.Reflection;
+using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces;
 using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.XmlValidation.DataValidation;
 using System.Collections.Generic;
 using System.Xml;
@@ -8,16 +10,29 @@ namespace ABIS.LogicBuilder.FlowBuilder.Services.XmlValidation.DataValidation
 {
     internal class LiteralParameterElementValidator : ILiteralParameterElementValidator
     {
-        private readonly IXmlElementValidator _xmlElementValidator;
+        private readonly IEnumHelper _enumHelper;
+        private readonly IExceptionHelper _exceptionHelper;
+        private readonly ILiteralElementValidator _literalElementValidator;
 
         public LiteralParameterElementValidator(IXmlElementValidator xmlElementValidator)
         {
-            _xmlElementValidator = xmlElementValidator;
+            _enumHelper = xmlElementValidator.ContextProvider.EnumHelper;
+            _exceptionHelper = xmlElementValidator.ContextProvider.ExceptionHelper;
+            _literalElementValidator = xmlElementValidator.LiteralElementValidator;
         }
 
         public void Validate(LiteralParameter parameter, XmlElement parameterElement, ApplicationTypeInfo application, List<string> validationErrors)
         {
-            //throw new System.NotImplementedException();
+            if (parameterElement.Name != XmlDataConstants.LITERALPARAMETERELEMENT)
+                throw _exceptionHelper.CriticalException("{E3BC3B76-B0BF-4936-B4AF-6C8F99B0F157}");
+
+            _literalElementValidator.Validate
+            (
+                parameterElement,
+                _enumHelper.GetSystemType(parameter.LiteralType),
+                application,
+                validationErrors
+            );
         }
     }
 }
