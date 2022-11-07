3e1133aec2e1a58e1e94f817ed65127aa7aee8ae
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-11-07 10:59:07 -0500

    ConfigureWebApiDeployment layout

---
+++ TelerikLogicBuilder/Configuration/Forms/ConfigureWebApiDeployment.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace ABIS.LogicBuilder.FlowBuilder.Configuration.Forms
+{
+    partial class ConfigureWebApiDeployment
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radPanelBottom = new Telerik.WinControls.UI.RadPanel();
+            this.radPanelMessages = new Telerik.WinControls.UI.RadPanel();
+            this.radPanelButtons = new Telerik.WinControls.UI.RadPanel();
+            this.tableLayoutPanelButtons = new System.Windows.Forms.TableLayoutPanel();
+            this.btnCancel = new Telerik.WinControls.UI.RadButton();
+            this.btnOk = new Telerik.WinControls.UI.RadButton();
+            this.radPanelTableParent = new Telerik.WinControls.UI.RadPanel();
+            this.tableLayoutPanel = new System.Windows.Forms.TableLayoutPanel();
+            this.radGroupBoxUrls = new Telerik.WinControls.UI.RadGroupBox();
+            this.radPanelUrls = new Telerik.WinControls.UI.RadPanel();
+            this.lblPostFileDataUrl = new Telerik.WinControls.UI.RadLabel();
+            this.lblDeleteRulesUrl = new Telerik.WinControls.UI.RadLabel();
+            this.lblDeleteAllRulesUrl = new Telerik.WinControls.UI.RadLabel();
+            this.txtPostFileDataUrl = new Telerik.WinControls.UI.RadTextBox();
+            this.txtDeleteRulesUrl = new Telerik.WinControls.UI.RadTextBox();
+            this.txtDeleteAllRulesUrl = new Telerik.WinControls.UI.RadTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelBottom)).BeginInit();
+            this.radPanelBottom.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelMessages)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelButtons)).BeginInit();
+            this.radPanelButtons.SuspendLayout();
+            this.tableLayoutPanelButtons.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCancel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnOk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelTableParent)).BeginInit();
+            this.radPanelTableParent.SuspendLayout();
+            this.tableLayoutPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxUrls)).BeginInit();
+            this.radGroupBoxUrls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelUrls)).BeginInit();
+            this.radPanelUrls.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.lblPostFileDataUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblDeleteRulesUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblDeleteAllRulesUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPostFileDataUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDeleteRulesUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDeleteAllRulesUrl)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radPanelBottom
+            // 
+            this.radPanelBottom.Controls.Add(this.radPanelMessages);
+            this.radPanelBottom.Controls.Add(this.radPanelButtons);
+            this.radPanelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.radPanelBottom.Location = new System.Drawing.Point(0, 201);
+            this.radPanelBottom.Name = "radPanelBottom";
+            this.radPanelBottom.Size = new System.Drawing.Size(943, 150);
+            this.radPanelBottom.TabIndex = 1;
+            // 
+            // radPanelMessages
+            // 
+            this.radPanelMessages.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radPanelMessages.Location = new System.Drawing.Point(0, 0);
+            this.radPanelMessages.Name = "radPanelMessages";
+            this.radPanelMessages.Size = new System.Drawing.Size(783, 150);
+            this.radPanelMessages.TabIndex = 2;
+            // 
+            // radPanelButtons
+            // 
+            this.radPanelButtons.Controls.Add(this.tableLayoutPanelButtons);
+            this.radPanelButtons.Dock = System.Windows.Forms.DockStyle.Right;
+            this.radPanelButtons.Location = new System.Drawing.Point(783, 0);
+            this.radPanelButtons.Name = "radPanelButtons";
+            this.radPanelButtons.Size = new System.Drawing.Size(160, 150);
+            this.radPanelButtons.TabIndex = 2;
+            // 
+            // tableLayoutPanelButtons
+            // 
+            this.tableLayoutPanelButtons.ColumnCount = 3;
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 70F));
+            this.tableLayoutPanelButtons.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanelButtons.Controls.Add(this.btnCancel, 1, 3);
+            this.tableLayoutPanelButtons.Controls.Add(this.btnOk, 1, 1);
+            this.tableLayoutPanelButtons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelButtons.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanelButtons.Name = "tableLayoutPanelButtons";
+            this.tableLayoutPanelButtons.RowCount = 9;
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelButtons.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 4F));
+            this.tableLayoutPanelButtons.Size = new System.Drawing.Size(160, 150);
+            this.tableLayoutPanelButtons.TabIndex = 0;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnCancel.Location = new System.Drawing.Point(27, 45);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(106, 24);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "&Cancel";
+            // 
+            // btnOk
+            // 
+            this.btnOk.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOk.Location = new System.Drawing.Point(27, 9);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(106, 24);
+            this.btnOk.TabIndex = 0;
+            this.btnOk.Text = "&Ok";
+            // 
+            // radPanelTableParent
+            // 
+            this.radPanelTableParent.Controls.Add(this.tableLayoutPanel);
+            this.radPanelTableParent.Dock = System.Windows.Forms.DockStyle.Top;
+            this.radPanelTableParent.Location = new System.Drawing.Point(0, 0);
+            this.radPanelTableParent.Name = "radPanelTableParent";
+            this.radPanelTableParent.Size = new System.Drawing.Size(939, 154);
+            this.radPanelTableParent.TabIndex = 3;
+            // 
+            // tableLayoutPanel
+            // 
+            this.tableLayoutPanel.ColumnCount = 4;
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 31F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 63F));
+            this.tableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 3F));
+            this.tableLayoutPanel.Controls.Add(this.txtDeleteAllRulesUrl, 2, 5);
+            this.tableLayoutPanel.Controls.Add(this.txtDeleteRulesUrl, 2, 3);
+            this.tableLayoutPanel.Controls.Add(this.txtPostFileDataUrl, 2, 1);
+            this.tableLayoutPanel.Controls.Add(this.lblDeleteAllRulesUrl, 1, 5);
+            this.tableLayoutPanel.Controls.Add(this.lblPostFileDataUrl, 1, 1);
+            this.tableLayoutPanel.Controls.Add(this.lblDeleteRulesUrl, 1, 3);
+            this.tableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel.Name = "tableLayoutPanel";
+            this.tableLayoutPanel.RowCount = 8;
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 30F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 6F / 154 * 100));
+            this.tableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F / 154 * 100));
+            this.tableLayoutPanel.Size = new System.Drawing.Size(939, 154);
+            this.tableLayoutPanel.TabIndex = 0;
+            // 
+            // radGroupBoxUrls
+            // 
+            this.radGroupBoxUrls.AccessibleRole = System.Windows.Forms.AccessibleRole.Grouping;
+            this.radGroupBoxUrls.Controls.Add(this.radPanelUrls);
+            this.radGroupBoxUrls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radGroupBoxUrls.HeaderText = "Urls";
+            this.radGroupBoxUrls.Location = new System.Drawing.Point(0, 0);
+            this.radGroupBoxUrls.Name = "radGroupBoxUrls";
+            this.radGroupBoxUrls.Size = new System.Drawing.Size(943, 201);
+            this.radGroupBoxUrls.TabIndex = 2;
+            this.radGroupBoxUrls.Text = "Urls";
+            // 
+            // radPanelUrls
+            // 
+            this.radPanelUrls.Controls.Add(this.radPanelTableParent);
+            this.radPanelUrls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.radPanelUrls.Location = new System.Drawing.Point(2, 18);
+            this.radPanelUrls.Name = "radPanelUrls";
+            this.radPanelUrls.Size = new System.Drawing.Size(939, 181);
+            this.radPanelUrls.TabIndex = 0;
+            // 
+            // lblPostFileDataUrl
+            // 
+            this.lblPostFileDataUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblPostFileDataUrl.Location = new System.Drawing.Point(31, 23);
+            this.lblPostFileDataUrl.Name = "lblPostFileDataUrl";
+            this.lblPostFileDataUrl.Size = new System.Drawing.Size(68, 18);
+            this.lblPostFileDataUrl.TabIndex = 0;
+            this.lblPostFileDataUrl.Text = "Post File Url:";
+            // 
+            // lblDeleteRulesUrl
+            // 
+            this.lblDeleteRulesUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDeleteRulesUrl.Location = new System.Drawing.Point(31, 59);
+            this.lblDeleteRulesUrl.Name = "lblDeleteRulesUrl";
+            this.lblDeleteRulesUrl.Size = new System.Drawing.Size(88, 18);
+            this.lblDeleteRulesUrl.TabIndex = 3;
+            this.lblDeleteRulesUrl.Text = "Delete Rules Url:";
+            // 
+            // lblDeleteAllRulesUrl
+            // 
+            this.lblDeleteAllRulesUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lblDeleteAllRulesUrl.Location = new System.Drawing.Point(31, 95);
+            this.lblDeleteAllRulesUrl.Name = "lblDeleteAllRulesUrl";
+            this.lblDeleteAllRulesUrl.Size = new System.Drawing.Size(104, 18);
+            this.lblDeleteAllRulesUrl.TabIndex = 3;
+            this.lblDeleteAllRulesUrl.Text = "Delete All Rules Url:";
+            // 
+            // txtPostFileDataUrl
+            // 
+            this.txtPostFileDataUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtPostFileDataUrl.Location = new System.Drawing.Point(322, 23);
+            this.txtPostFileDataUrl.Name = "txtPostFileDataUrl";
+            this.txtPostFileDataUrl.Size = new System.Drawing.Size(585, 20);
+            this.txtPostFileDataUrl.TabIndex = 0;
+            // 
+            // txtDeleteRulesUrl
+            // 
+            this.txtDeleteRulesUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtDeleteRulesUrl.Location = new System.Drawing.Point(322, 59);
+            this.txtDeleteRulesUrl.Name = "txtDeleteRulesUrl";
+            this.txtDeleteRulesUrl.Size = new System.Drawing.Size(585, 20);
+            this.txtDeleteRulesUrl.TabIndex = 3;
+            // 
+            // txtDeleteAllRulesUrl
+            // 
+            this.txtDeleteAllRulesUrl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtDeleteAllRulesUrl.Location = new System.Drawing.Point(322, 95);
+            this.txtDeleteAllRulesUrl.Name = "txtDeleteAllRulesUrl";
+            this.txtDeleteAllRulesUrl.Size = new System.Drawing.Size(585, 20);
+            this.txtDeleteAllRulesUrl.TabIndex = 3;
+            // 
+            // ConfigureWebApiDeployment
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(9, 21);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 351);
+            this.Controls.Add(this.radGroupBoxUrls);
+            this.Controls.Add(this.radPanelBottom);
+            this.Name = "ConfigureWebApiDeployment";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "ConfigureWebApiDeployment";
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelBottom)).EndInit();
+            this.radPanelBottom.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelMessages)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelButtons)).EndInit();
+            this.radPanelButtons.ResumeLayout(false);
+            this.tableLayoutPanelButtons.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnCancel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnOk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelTableParent)).EndInit();
+            this.radPanelTableParent.ResumeLayout(false);
+            this.tableLayoutPanel.ResumeLayout(false);
+            this.tableLayoutPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.radGroupBoxUrls)).EndInit();
+            this.radGroupBoxUrls.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.radPanelUrls)).EndInit();
+            this.radPanelUrls.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.lblPostFileDataUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblDeleteRulesUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.lblDeleteAllRulesUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPostFileDataUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDeleteRulesUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDeleteAllRulesUrl)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.UI.RadPanel radPanelBottom;
+        private Telerik.WinControls.UI.RadPanel radPanelMessages;
+        private Telerik.WinControls.UI.RadPanel radPanelButtons;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelButtons;
+        private Telerik.WinControls.UI.RadButton btnCancel;
+        private Telerik.WinControls.UI.RadButton btnOk;
+        private Telerik.WinControls.UI.RadPanel radPanelTableParent;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel;
+        private Telerik.WinControls.UI.RadGroupBox radGroupBoxUrls;
+        private Telerik.WinControls.UI.RadPanel radPanelUrls;
+        private Telerik.WinControls.UI.RadTextBox txtDeleteAllRulesUrl;
+        private Telerik.WinControls.UI.RadTextBox txtDeleteRulesUrl;
+        private Telerik.WinControls.UI.RadTextBox txtPostFileDataUrl;
+        private Telerik.WinControls.UI.RadLabel lblDeleteAllRulesUrl;
+        private Telerik.WinControls.UI.RadLabel lblPostFileDataUrl;
+        private Telerik.WinControls.UI.RadLabel lblDeleteRulesUrl;
+    }
+}
---
+++ TelerikLogicBuilder/Configuration/Forms/ConfigureWebApiDeployment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.Primitives;
+
+namespace ABIS.LogicBuilder.FlowBuilder.Configuration.Forms
+{
+    public partial class ConfigureWebApiDeployment : Telerik.WinControls.UI.RadForm
+    {
+        public ConfigureWebApiDeployment()
+        {
+            InitializeComponent();
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            ((BorderPrimitive)radPanelUrls.PanelElement.Children[1]).Visibility = ElementVisibility.Collapsed;
+            ((BorderPrimitive)radPanelTableParent.PanelElement.Children[1]).Visibility = ElementVisibility.Collapsed;
+        }
+    }
+}
