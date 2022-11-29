038ba61e60d93d75b3a20ec17592babba6313f32
Blaise Taylor <btaylor@bPolicySolutions.com>
2022-11-29 15:35:59 -0500

    Removing AnchorStyles from SelectRulesResourcesPairForm

--- TelerikLogicBuilder/RulesGenerator/Forms/SelectRulesResourcesPairForm.cs
+++ TelerikLogicBuilder/RulesGenerator/Forms/SelectRulesResourcesPairForm.cs
@@ -1,16 +1,20 @@
-﻿using ABIS.LogicBuilder.FlowBuilder.Constants;
-using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces;
+﻿using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces;
 using ABIS.LogicBuilder.FlowBuilder.ServiceInterfaces.TreeViewBuiilders;
 using ABIS.LogicBuilder.FlowBuilder.Structures;
 using ABIS.LogicBuilder.FlowBuilder.UserControls;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using Telerik.WinControls;
+using Telerik.WinControls.Primitives;
+using Telerik.WinControls.UI;
 
 namespace ABIS.LogicBuilder.FlowBuilder.RulesGenerator.Forms
 {
-    internal partial class SelectRulesResourcesPairForm : Telerik.WinControls.UI.RadForm, ISelectRulesResourcesPairForm
+    internal partial class SelectRulesResourcesPairForm : RadForm, ISelectRulesResourcesPairForm
     {
         private readonly string _applicationName;
         private readonly IFormInitializer _formInitializer;
@@ -36,7 +40,7 @@ namespace ABIS.LogicBuilder.FlowBuilder.RulesGenerator.Forms
 
         public IList<RulesResourcesPair> SourceFiles => _getAllCheckedNodeNames.GetNodes(radTreeView.Nodes[0])
                                                                 .Select(n => (RulesResourcesPair)n.Tag)
-                                                                .ToArray();
+        .ToArray();
 
         public void SetTitle(string title)
         {
@@ -44,39 +48,35 @@ namespace ABIS.LogicBuilder.FlowBuilder.RulesGenerator.Forms
             this.radGroupBoxTop.Text = title;
         }
 
+        private static void CollapsePanelBorder(RadPanel radPanel)
+            => ((BorderPrimitive)radPanel.PanelElement.Children[1]).Visibility = ElementVisibility.Collapsed;
+
         private void Initialize()
         {
             InitializeDialogFormMessageControl();
-            radButtonOk.Anchor = AnchorConstants.AnchorsLeftTopRight;
-            radButtonCancel.Anchor = AnchorConstants.AnchorsLeftTopRight;
             _formInitializer.SetFormDefaults(this, 648);
             _selectModulesForDeploymentTreeViewBuilder.Build(radTreeView, _applicationName);
 
-            radButtonOk.DialogResult = DialogResult.OK;
-            radButtonCancel.DialogResult = DialogResult.Cancel;
+            btnOk.DialogResult = DialogResult.OK;
+            btnCancel.DialogResult = DialogResult.Cancel;
+
+            CollapsePanelBorder(radPanelBottom);
+            CollapsePanelBorder(radPanelButtons);
+            CollapsePanelBorder(radPanelMessages);
+            CollapsePanelBorder(radPanelTop);
         }
 
         private void InitializeDialogFormMessageControl()
         {
-            ((System.ComponentModel.ISupportInitialize)(this.radSplitContainerMain)).BeginInit();
-            this.radSplitContainerMain.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitPanelBottom)).BeginInit();
-            this.splitPanelBottom.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.radPanelMessages)).BeginInit();
+            ((ISupportInitialize)(this.radPanelMessages)).BeginInit();
             this.radPanelMessages.SuspendLayout();
-            this.SuspendLayout();
 
             _dialogFormMessageControl.Dock = DockStyle.Fill;
-            _dialogFormMessageControl.Location = new System.Drawing.Point(0, 0);
+            _dialogFormMessageControl.Location = new Point(0, 0);
             this.radPanelMessages.Controls.Add((Control)_dialogFormMessageControl);
 
-            ((System.ComponentModel.ISupportInitialize)(this.radSplitContainerMain)).EndInit();
-            this.radSplitContainerMain.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitPanelBottom)).EndInit();
-            this.splitPanelBottom.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.radPanelMessages)).EndInit();
-            this.radPanelMessages.ResumeLayout(false);
-            this.ResumeLayout(false);
+            ((ISupportInitialize)(this.radPanelMessages)).EndInit();
+            this.radPanelMessages.ResumeLayout(true);
         }
     }
 }
