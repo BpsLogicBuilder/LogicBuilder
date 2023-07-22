11367847645a229f22a8e834b598ab2b6e69d3ed
Blaise Taylor <btaylor@bPolicySolutions.com>
2023-07-22 10:08:34 -0400

    Version 2.0.13.0.  Assembly v3.0.13.0.

--- TelerikLogicBuilder/Properties/AssemblyInfo.cs
+++ TelerikLogicBuilder/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("2.0.22.0")]
 [assembly: AssemblyFileVersion("2.0.22.0")]
 #else
-[assembly: AssemblyVersion("3.0.12.0")]
-[assembly: AssemblyFileVersion("3.0.12.0")]
+[assembly: AssemblyVersion("3.0.13.0")]
+[assembly: AssemblyFileVersion("3.0.13.0")]
 #endif
